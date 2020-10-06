79271e5692c157110e5fefc477f1024aa6f9c056
Alexander Lonsky <alexander.lonsky@gmail.com>
2020-10-06 20:33:15 +0300

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GiphyUISDK",
+    products: [
+        .library(
+            name: "GiphyUISDK",
+            targets: ["GiphyUISDK"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GiphyUISDK",
+            path: "GiphyUISDK.xcframework")
+    ]
+)
