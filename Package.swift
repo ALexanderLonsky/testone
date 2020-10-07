0e514b03992cce082cd1c53ad10937cf37a1365c
Alexander Lonsky <alexander.lonsky@gmail.com>
2020-10-07 13:25:55 +0300

    more

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
                .target(name: "GiphyUISDK"),
                 "DeepDiff", "PINCache"
             ],
-            path: "GiphyUISDKWrapper"      
+            path: "Source"      
         ),    
     ]
 )
