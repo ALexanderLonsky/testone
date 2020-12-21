0984a1f20c763ca8437f4f076d2bbee961ebb45d
Alexander Lonsky <alexander.lonsky@gmail.com>
2020-12-21 20:07:35 +0200

    1.0.34

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
             targets: ["GiphyUISDKWrapper"]),
     ],
     dependencies: [
-        .package(name: "GiphyCoreSDK", url: "https://github.com/ALexanderLonsky/testonecor.git", from: "1.0.1")
+        .package(name: "GiphyCoreSDK", url: "https://github.com/ALexanderLonsky/testonecor.git", from: "1.0.1"),
+        .package(name: "PINCache", url: "https://github.com/pinterest/PINCache", from: "3.0.1"),
     ],
     targets: [
         .binaryTarget(
@@ -20,7 +21,7 @@ let package = Package(
         .target(
             name: "GiphyUISDKWrapper",
             dependencies: [
-               .target(name: "GiphyUISDK"), "GiphyCoreSDK"
+                .target(name: "GiphyUISDK"), "PINCache", "GiphyCoreSDK"
             ],
             path: "./Sources"      
         ),    
